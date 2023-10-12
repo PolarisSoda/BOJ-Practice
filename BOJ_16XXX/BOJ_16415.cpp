93958393b1bf4d36d3269ece74043e9d4ce33494
PolarisSoda <urin187@gmail.com>
2023-10-12 13:18:03 +0900

    Create BOJ_16415.cpp

---
+++ BOJ_16XXX/BOJ_16415.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int LEN = 100001;
+
+int SA[LEN],G[LEN],temp[LEN],LCP[LEN],t;
+
+bool comp(int x,int y) {
+    if(G[x] == G[y]) return G[x+t] < G[y+t];
+    return G[x] < G[y];
+}
+void GetSuffix(string str,int len) {
+    for(int i=0; i<len; i++) {
+        SA[i] = i;
+        G[i] = str[i];
+    }
+    t = 1;
+    while(t <= len) {
+        G[len] = -1;
+        sort(SA,SA+len,comp);
+        temp[SA[0]] = 0;
+        for(int i=1; i<len; i++) {
+            temp[SA[i]] = temp[SA[i-1]];
+            if(comp(SA[i-1],SA[i])) temp[SA[i]]++;
+        }
+        for(int i=0; i<len; i++) G[i] = temp[i];
+        t <<= 1;
+    }
+}
+
+void GetLCP(string str,int len) {
+    for(int i=0, k=0; i<len; i++, k=max(k-1,0)) {
+        if(G[i] == len-1) continue;
+        for(int j=SA[G[i]+1]; str[i+k] == str[j+k]; k++);
+        LCP[G[i]] = k;
+    }
+}
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string str;
+    cin >> str;
+    int len = str.length();
+    GetSuffix(str,len);
+    GetLCP(str,len);
+
+    int maxv = 0;
+    string strans;
+    for(int i=0; i<len-1; i++) {
+            if(LCP[i] > maxv) {
+            maxv = LCP[i];
+            strans = str.substr(SA[i],LCP[i]);
+        }
+    }
+    cout << strans;
+    return 0;
+}
