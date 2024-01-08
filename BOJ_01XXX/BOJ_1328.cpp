4391eedf9a3f20bdfaaa11f5eac06e77984e46b3
PolarisSoda <urin187@gmail.com>
2024-01-08 14:58:59 +0900

    Update BOJ_1328.cpp
    I solved it!

--- BOJ_01XXX/BOJ_1328.cpp
+++ BOJ_01XXX/BOJ_1328.cpp
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
-#define DIV 1000000007
 
 using namespace std;
 
 typedef long long ll;
 
+constexpr int DIV = 1000000007;
 ll DP[101][101][101];
-
-ll Dynamic(int left,int num,int right) {
-    if(DP[left][num][right] != -1) return DP[left][num][right];
-
-    ll ans = 0;
-    for(int i=1; i<=num; i++) {
-        int l = Dynamic(1,1,1);
-        int r = Dynamic(1,1,1);
-        if(l == 0 || r == 0) {
-            return 0;
-        }
-    }
-    return 0;
-}
 
 int main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
+    DP[1][1][1] = 1;
+    DP[2][2][1] = 1;
+    DP[1][2][2] = 1;
+    for(int i=3; i<=100; i++) {
+        for(int j=1; j<=i; j++) {
+            for(int k=1; k<=i; k++) {
+                if(i+j == 2 || j+k > i+1) continue;
+                DP[j][i][k] = DP[j-1][i-1][k] + DP[j][i-1][k-1] + DP[j][i-1][k]*(i-2);
+                DP[j][i][k] %= DIV;
+            }
+        }
+    }
+
     int N,L,R;
     cin >> N >> L >> R;
-
-    cout << Dynamic(L,N,R);
+    cout << DP[L][N][R];
+    exit(0);
 }
