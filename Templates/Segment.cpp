4086513347a49f3dc623d87fa474fec30bfaaf1e
PolarisSoda <urin187@gmail.com>
2024-03-10 14:09:10 +0900

    Update Segment.cpp

--- Templates/Segment.cpp
+++ Templates/Segment.cpp
@@ -6,25 +6,20 @@ int len;
 vector<int> ST;
 
 void Init() {
-    for(int i=len-1; i>0; i--) {
+    for(int i=len-1; i>0; i--)
         ST[i] = ST[i<<1] + ST[i<<1|1];
-    }
 }
 int Query(int left,int right) {
     int ans = 0;
     for(left+=len,right+=len; left<right; left>>=1,right>>=1) {
-        if(left&1)
-            ans += ST[left++];
-        if(right&1)
-            ans += ST[--right];
+        if(left&1) ans += ST[left++];
+        if(right&1) ans += ST[--right];
     }
     return ans;
 }
-
 void Update(int lo,int value) {
-    for(ST[lo+=len] = value; lo>1; lo>>=1) {
+    for(ST[lo+=len] = value; lo>1; lo>>=1)
         ST[lo>>1] = ST[lo] + ST[lo^1];
-    }
 }
 
 int main() {
